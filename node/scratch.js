13a5ee70a2033fdcf4930ecd4ab37c6e9a1debda
kcanamar <99228411+kcanamar@users.noreply.github.com>
2025-04-01 19:10:03 -0700

    complete first break, prep work

---
+++ node/scratch.js
@@ -0,0 +1,29 @@
+
+// Thinking in Javascript
+
+// This is a comment, a single line comment
+/* 
+ * This is a multiline comment
+ * here is more of that same comment
+ * */
+
+// How to print to the console, the second most important thing to learn in any language.
+console.log("Im a real boy")
+
+// example of hoisting in action
+console.log(thing = "something")
+
+// Variables
+// Declared, using very specific words. 
+
+// let - a mutable variable, the value can be change. This may be reassigned
+
+// = is the assignment operator, this does not represent equality
+let myName = "Kyle" // String begins with ",',` and contains characters 
+
+// const - is an immutable variable, meaning it cannot be reassigned
+const theSky = "is blue"
+
+// var - is mutable, and is contained in the global scope.
+// It receives the super power of hoisting 
+var thing = "hoisted"
