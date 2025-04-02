0bd54996d5a44de99fd7ec5180dd6d4bfa7e6b67
kcanamar <99228411+kcanamar@users.noreply.github.com>
2025-04-01 20:02:47 -0700

    complete js intro

--- node/scratch.js
+++ node/scratch.js
@@ -27,3 +27,17 @@ const theSky = "is blue"
 // var - is mutable, and is contained in the global scope.
 // It receives the super power of hoisting 
 var thing = "hoisted"
+
+
+// Primitive values, are the most generic values. Types of data in their simplest form
+console.log(typeof myName, "<<<<<< This is me checking the variable myName")
+console.log("This is a >>", typeof 1)
+console.log("This is a >>", typeof 12.3)
+console.log("This is a >>", typeof true)
+console.log("This is a >>", typeof false)
+console.log("This is >>", typeof null) // This is the developer setting this value explicitly
+console.log("This is >>", null) // This is the developer setting this value explicitly
+console.log("This is >>", typeof undefined) // This is random
+
+// Everything in javascript is true, except
+// "", 0, null, undefined, false, NaN - Not a Number
